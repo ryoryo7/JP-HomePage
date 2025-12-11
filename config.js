6fd9a73bc8b80296f0d31896ebac635b0cef35c1
ryoryo7 <ryotaro.hoso@gmail.com>
2025-12-11 18:20:06 +0900

    ページ追加

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // Form endpoint configuration
-// 右リンクの、「メール送信」スクリプトを参照。https://script.google.com/home?hl=ja 
+// 右リンクの、「メール送信-国内LP版」スクリプトを参照。https://script.google.com/home?hl=ja 
 window.JBOUNDEX_CONFIG = {
-    formAction: "https://script.google.com/macros/s/AKfycbyqNbOKGMoW_oIHj-wAGaDP46Bv2YdAo-ADn8Ag3KcXHrPZHucBi2VNkL2tkeb5Rjgm/exec"
+    formAction: "https://script.google.com/macros/s/AKfycbz_CJGuPq47jBYeVk0NZkQLnKth1pH9PhkNX-EZdKm1OhK3_zmk_oNEuybhCBQWN9v-DA/exec"
 };
